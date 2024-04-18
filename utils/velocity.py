09de5db2758c6a0f62135c1e809ce5dc1a8c60ea
fernaldyl <90769501+fernaldy112@users.noreply.github.com>
2024-04-18 20:40:29 +0700

    feat: optical flow visualizer

--- utils/velocity.py
+++ utils/velocity.py
@@ -1,12 +1,10 @@
 import cv2
 import numpy as np
 
-def lk_optical_flow(old_frame, new_frame, old_points, clear_frame):  
-  lk_params = dict(winSize = (15,15), 
+def lk_optical_flow(old_frame, new_frame, old_points):  
+  lk_params = dict(winSize = (50, 50), 
                    maxLevel = 2, 
                    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-  color = np.random.randint(0, 255, (100, 3))
-  mask = np.zeros_like(old_frame)
   
   new_points, status, error = cv2.calcOpticalFlowPyrLK(old_frame, new_frame, old_points, None, **lk_params)
   
@@ -17,18 +15,21 @@ def lk_optical_flow(old_frame, new_frame, old_points, clear_frame):
   speeds = np.linalg.norm(velocities, axis=1)
   average_speed = np.average(speeds)
   
-  return average_speed
+  return good_new, good_old, average_speed
+
+def visualize_lk(clear_frame, good_new, good_old):
+  color = np.random.randint(0, 255, (100, 3))
+  mask = np.zeros_like(clear_frame)
+
+  frame = mask.copy() 
   
-  # TODO: delete
-  # frame = clear_frame.copy() 
-  # for i, (new, old) in enumerate(zip(good_new, good_old)):
-  #   a, b = new.ravel()
-  #   c, d = old.ravel()
-  #   a, b, c, d = int(a), int(b), int(c), int(d)
-  #   mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
-  #   frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+  for i, (new, old) in enumerate(zip(good_new, good_old)):
+    new_x, new_y = new.ravel()
+    old_x, old_y = old.ravel()
+    new_x, new_y, old_x, old_y = int(new_x), int(new_y), int(old_x), int(old_y)
+    mask = cv2.line(mask, (new_x, new_y), (old_x, old_y), color[i].tolist(), 2)
+    frame = cv2.circle(frame, (new_x, new_y), 5, color[i].tolist(), -1)
     
-  # mask_color = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-  # img = cv2.add(frame, mask_color)
+  img = cv2.add(frame, mask)
     
-  # return img
+  return img
