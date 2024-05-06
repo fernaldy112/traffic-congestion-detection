3827b9393b3b2869fab5f6ab8e229f844f4af7a4
fernaldyl <90769501+fernaldy112@users.noreply.github.com>
2024-05-06 15:18:48 +0700

    fix: guard divide by zero in ratio calculation

--- utils/occupancy.py
+++ utils/occupancy.py
@@ -1,5 +1,3 @@
-from ultralytics.engine.results import Boxes
-
 def ratio_pixel(vehicle_pixels: float, road_pixels: int) -> float:
   """
   Returns ratio between area of vehicles and area of the road.
@@ -16,4 +14,6 @@ def ratio_pixel(vehicle_pixels: float, road_pixels: int) -> float:
   float
     Division result between sum of area of all boxes and area of the road.
   """
-  return min(1, vehicle_pixels / road_pixels)
+  if road_pixels == 0:
+    return 0
+  return vehicle_pixels / road_pixels
